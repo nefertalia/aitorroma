02ce1fcdb25f44b9d3e3d8e776a1c79aafeace89
cmm-user <tuxed@redaven.com>
2022-03-11 22:58:43 +0100

    Add Plausible and set version

--- project/custom-script.js
+++ project/custom-script.js
@@ -1,15 +1,3 @@
-<!-- Matomo -->
-<script type="text/javascript">
-  var _paq = window._paq = window._paq || [];
-  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-  _paq.push(['trackPageView']);
-  _paq.push(['enableLinkTracking']);
-  (function() {
-    var u="https://analytics.comunidad-n8n.com/";
-    _paq.push(['setTrackerUrl', u+'matomo.php']);
-    _paq.push(['setSiteId', '4']);
-    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.type='text/javascript'; g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
-  })();
-</script>
-<!-- End Matomo Code -->
+<!-- Plausible -->
+<script defer data-domain="aitorroma.com" src="https://plausible.hiveagile.dev/js/script.js"></script>
+
